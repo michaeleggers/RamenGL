24e9e45a63945d07ecbd68ed71eae7e9235dd131
Michael Eggers <me@pythno.org>
2026-03-23 16:09:11 +0100

    Move SDL/GL/Imgui init/shutdown into ramen.h

--- task01.cpp
+++ task01.cpp
@@ -14,15 +14,12 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_opengl.h>
 
+#include "ramen/ramen.h"
 #include "ramen/rgl_camera.h"
 #include "ramen/rgl_defines.h"
 #include "ramen/rgl_math.h"
 #include "ramen/rgl_model.h"
 #include "ramen/rgl_shader.h"
-
-static SDL_Window*   g_pWindow;
-static SDL_Renderer* g_pRenderer;
-static SDL_GLContext g_glContext;
 
 static std::vector<Vertex> g_Cone;
 static std::vector<Vertex> g_Cap;
@@ -95,82 +92,8 @@ void CreateGeometry()
 
 int main(int argc, char** argv)
 {
-    Mat4f M1    = Mat4f::Identity();
-    Mat4f M2    = Mat4f::Identity();
-    Mat4f M1xM2 = M1 * M2;
-    printf("M1xM2:\n%s\n", M1xM2.ToString());
-    Vec4f v4    = Vec4f{ 1, 2, 3, 4 };
-    Vec4f M1xV4 = M1 * v4;
-    printf("M1xV4: \n%s\n", M1xV4.ToString());
-
-    if ( !SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) )
-    {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not initialize SDL. Reason: %s\n", SDL_GetError());
-        exit(1);
-    }
-
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
-    g_pWindow = SDL_CreateWindow("Task01", 800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-    if ( !g_pWindow )
-    {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window. Reason: %s\n", SDL_GetError());
-        exit(1);
-    }
-
-    g_glContext = SDL_GL_CreateContext(g_pWindow);
-    if ( !g_glContext )
-    {
-        SDL_LogError(
-            SDL_LOG_CATEGORY_ERROR, "Could not create OpenGL context for window. Reason: %s\n", SDL_GetError());
-        exit(1);
-    }
-
-    if ( !gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress) )
-    {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not load OpenGL extensions via GLAD.");
-        exit(1);
-    }
-
-    if ( !SDL_GL_MakeCurrent(g_pWindow, g_glContext) )
-    {
-        SDL_LogError(
-            SDL_LOG_CATEGORY_ERROR, "Could not create OpenGL context for window. Reason: %s\n", SDL_GetError());
-        exit(1);
-    }
-
-    /* Print GL version */
-    printf("Got OpenGL Version: %s\n", glGetString(GL_VERSION));
-    printf("Got OpenGL Renderer: %s\n", glGetString(GL_RENDERER));
-
-    /* Init Dear ImGUI */
-    /* ImGUI context */
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
-
-    /* Setup Dear ImGui style */
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
-
-    /* ImGUI scaling */
-    float       mainScale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
-    ImGuiStyle& style     = ImGui::GetStyle();
-    style.ScaleAllSizes(
-        mainScale); // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
-    style.FontScaleDpi
-        = mainScale; // Set initial font scale. (in docking branch: using io.ConfigDpiScaleFonts=true automatically overrides this for every window depending on the current monitor)
-
-    /* Setup Platform/Renderer backends */
-    ImGui_ImplSDL3_InitForOpenGL(g_pWindow, g_glContext);
-    ImGui_ImplOpenGL3_Init("#version 460");
+    Ramen* pRamen = Ramen::Create();
+    pRamen->Init("Task 02", 800, 600);
 
     /* Load shaders. */
     Shader shader{};
@@ -196,27 +119,6 @@ int main(int argc, char** argv)
     /* Model mat*/
     Mat4f modelMat = Scale(Vec3f{ 3.0f, 3.0f, 3.0f });
     Translate(modelMat, Vec3f{ 0.0f, 0.0f, -20.0f }); //Mat4f::Identity();
-
-#if 0
-    printf("viewMat: \n%s\n", viewMat.ToString());
-    const float* viewMatData = viewMat.Data();
-    printf("viewMatData: %f\n", viewMatData[ 0 ]);
-    printf("viewMatData: %f\n", viewMatData[ 1 ]);
-    printf("viewMatData: %f\n", viewMatData[ 2 ]);
-    printf("viewMatData: %f\n", viewMatData[ 3 ]);
-    printf("viewMatData: %f\n", viewMatData[ 4 ]);
-    printf("viewMatData: %f\n", viewMatData[ 5 ]);
-    printf("viewMatData: %f\n", viewMatData[ 6 ]);
-    printf("viewMatData: %f\n", viewMatData[ 7 ]);
-    printf("viewMatData: %f\n", viewMatData[ 8 ]);
-    printf("viewMatData: %f\n", viewMatData[ 9 ]);
-    printf("viewMatData: %f\n", viewMatData[ 10 ]);
-    printf("viewMatData: %f\n", viewMatData[ 11 ]);
-    printf("viewMatData: %f\n", viewMatData[ 12 ]);
-    printf("viewMatData: %f\n", viewMatData[ 13 ]);
-    printf("viewMatData: %f\n", viewMatData[ 14 ]);
-    printf("viewMatData: %f\n", viewMatData[ 15 ]);
-#endif
 
     /* VAO. */
     GLuint VAO;
@@ -269,7 +171,7 @@ int main(int argc, char** argv)
 
         /* Query new frame dimensions */
         int windowWidth, windowHeight;
-        SDL_GetWindowSize(g_pWindow, &windowWidth, &windowHeight);
+        SDL_GetWindowSize(pRamen->GetWindow(), &windowWidth, &windowHeight);
 
         /* Adjust viewport and perspective projection accordingly. */
         glViewport(0, 0, windowWidth, windowHeight);
@@ -305,21 +207,15 @@ int main(int argc, char** argv)
 
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        SDL_GL_SwapWindow(g_pWindow);
+        SDL_GL_SwapWindow(pRamen->GetWindow());
     }
 
     /* GL Resources shutdown. */
     shader.Delete();
     glDeleteVertexArrays(1, &VAO);
 
-    /* ImGUI Shutdown */
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplSDL3_Shutdown();
-    ImGui::DestroyContext();
-    /* SDL Shutdown */
-    SDL_GL_DestroyContext(g_glContext);
-    SDL_DestroyWindow(g_pWindow);
-    SDL_Quit();
+    /* Ramen Shutdown */
+    pRamen->Shutdown();
 
     return 0;
 }
